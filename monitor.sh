610f407f12381658abbfc5736dc40983e898dca5
Amanza17 <157617646+Amanza17@users.noreply.github.com>
2026-03-16 20:36:46 +0100

    base

--- monitor.sh
+++ monitor.sh
@@ -2,13 +2,28 @@
 # DASHBOARD DE CONTROL - ROVER 2026
 NC='\033[0m'; CYAN='\033[0;36m'; YELLOW='\033[1;33m'; RED='\033[0;31m'; GREEN='\033[0;32m'
 LOG_FILE="rover_system.log"
-touch $LOG_FILE
+
 # UMBRALES DE SEGURIDAD (Thresholds)
-MAX_TEMP=50
-MIN_BATT=15
-MAX_RPM=3000
+MAX_TEMP=0
+MIN_BATT=0
+MAX_RPM=0
+
+MODE="all"
+AVERAGE="false"
+
+#Para opcional flags
+
+#for arg in "$@"; do
+#    case "$arg" in
+#        -t) MODE="temp" ;;
+#
+#   esac
+#done
+
+
 clear
 echo -e "${CYAN}>>> MONITOR INICIADO. ESPERANDO TELEMETRÍA... <<<\${NC}"
+touch $LOG_FILE
 
 # Leer el log en tiempo real línea a línea
 tail -f $LOG_FILE | while read -r line; do
@@ -18,60 +33,19 @@ tail -f $LOG_FILE | while read -r line; do
     echo -e "${CYAN}==================================================${NC}"
     
     # --- TAREA ALUMNOS: Extraer datos con grep y awk ---
-    # --- TAREA ALUMNOS: Extraer datos con grep y awk ---
-    
-    # Extraemos la temperatura (Última línea que contenga [TEMP])
-    TEMP=$(grep "\[TEMP\]" $LOG_FILE | tail -n 1 | awk '{print $NF}')
-    
-    # Extraemos las RPM (Última línea que contenga [MOTORES])
-    RPM=$(grep "\[MOTORES\]" $LOG_FILE | tail -n 1 | awk '{print $NF}')
-    
-    # Extraemos la Batería (Última línea que contenga [BATT])
-    BATT=$(grep "\[BATT\]" $LOG_FILE | tail -n 1 | awk '{print $NF}')
-
-    # --- RENDERIZADO DE DATOS ---
-    echo -e "  TERMÓMETRO: ${YELLOW}${TEMP:-'--'}°C${NC}"
-    echo -e "  MOTORES:     ${CYAN}${RPM:-'--'} RPM${NC}"
-    echo -e "  ENERGÍA:    ${GREEN}${BATT:-'--'}%${NC}"
-    # --- TAREA ALUMNOS: Lógica de Watchdog para 'CRITICAL' ---
-    
-# --- TAREA ALUMNOS: Lógica de Watchdog por Umbrales ---
-
-    # 1. Validar Temperatura
-    if [ "${TEMP:-0}" -gt "$MAX_TEMP" ]; then
-        echo -e "${RED}[!] ALERTA: Temperatura crítica: ${TEMP}°C (Límite: $MAX_TEMP)${NC}"
-        echo "$(date +%T) [WATCHDOG] EMERGENCIA: Sobrecalentamiento detectado" >> $LOG_FILE
-            # Detener el generador de datos si hay peligro
-        pkill -f data_generator.sh
-        echo -e "${RED}SISTEMA DETENIDO POR SEGURIDAD.${NC}"
-        exit
-
-    fi
-
-    # 2. Validar Batería
-    if [ "${BATT:-100}" -lt "$MIN_BATT" ]; then
-        echo -e "${RED}[!] ALERTA: Batería baja: ${BATT}% (Límite: $MIN_BATT)${NC}"
-        echo "$(date +%T) [WATCHDOG] AVISO: Nivel de energía crítico" >> $LOG_FILE
-            # Detener el generador de datos si hay peligro
-        pkill -f data_generator.sh
-        echo -e "${RED}SISTEMA DETENIDO POR SEGURIDAD.${NC}"
-        exit
-
-    fi
-
-    # 3. Validar Motores
-    if [ "${RPM:-0}" -gt "$MAX_RPM" ]; then
-        echo -e "${RED}[!] ALERTA: RPM excesivas: ${RPM} (Límite: $MAX_RPM)${NC}"
-        echo "$(date +%T) [WATCHDOG] EMERGENCIA: Exceso de velocidad en motores" >> $LOG_FILE
-            # Detener el generador de datos si hay peligro
-        pkill -f data_generator.sh
-        echo -e "${RED}SISTEMA DETENIDO POR SEGURIDAD.${NC}"
-        exit
-    fi
 
 
+    echo -e "${CYAN}--------------------------------------------------${NC}"
+
+
+    echo -e "  TEMP:    ${YELLOW}${MAX_TEMP:-'--'}°C${NC}" #sustituit por los valores correctos
+    echo -e "  ENERGÍA: ${GREEN}${MAX_RPM:-'--'}%${NC}" #sustituit por los valores correctos
+    echo -e "  RPM:     ${CYAN}${MAX_BAT:-'--'} RPM${NC}" #sustituit por los valores correctos
+
+    echo -e "${CYAN}--------------------------------------------------${NC}"
     echo -e "  ULTIMA LÍNEA RECIBIDA: $line"
     echo -e "${CYAN}--------------------------------------------------${NC}"
+
     
-    # --- TAREA ALUMNOS: Lógica de Watchdog para 'CRITICAL' ---
+    
 done
